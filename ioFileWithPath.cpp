69bc1fe86ce397bc9785d2b43a74fd4a454af605
envikaaa <envikapriyatna31@gmail.com>
2026-06-01 15:53:15 +0700

    menambahkan if

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -28,5 +28,7 @@ int main()
         // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
 
+        // kondisi jika baris hanya berisi huruf q
+        if (baris == "q")
     }
 }
